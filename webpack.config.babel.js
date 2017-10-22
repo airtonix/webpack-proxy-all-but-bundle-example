9999e3691bbbd7d1c1711282ab2797546e6602f1
Zenobius Jiricek <airtonix@gmail.com>
2017-10-22 13:15:55 +1030

    Fix: add commentary to clear up

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -28,26 +28,51 @@ module.exports = (env) => {
       proxy: {
         "/": {
 
+          // never gets used. proxy won't use bypass or router if this isn't defined.
           target: 'http://localhost/backend/',
 
+          /**
+           * Route the request onto a specified host
+           * @param {http.request} request 
+           * @returns {string} 'scheme://host[:port]/' to forward requests to.
+           */
           router (request) {
               const {host} = request.headers;
-              const target = `http://${host.replace(`:${env.port}`, ``)}:5000`;
+
+              // in our case we just want to change the port
+              // But you could do table lookups, pathname tests, time of day... whatever
+              // return value has to be of the format: 'scheme://host:port'
+              const target = `http://${host.replace(`:${env.port}`, `5000`)}`;
+
               log('proxy.router', {host, target});
+
               return target;
           },
 
+          /**
+           * 
+           * @param {http.request} request 
+           * @param {http.response} response 
+           * @param {Object} options devServer.proxy object
+           * @returns {String|Boolean} Falsey to continue proxy behaviour, pathname to serve from webpack dev server.
+           */
           bypass (request, response, options) {
               // test if request.path is for something webpack-dev-server would serve.
               // otherwise return false, meaning we keep going through proxy to the backend.
               const asset = request.path;
+
+              // yesno if the requested path is one of the following.
               const byPass = [
                   env.public,
                   '/webpack-dev-server/',
                   '/sockjs-node',
                   '/socket.io/',
               ].find(pattern => asset.indexOf(pattern) >= 0 ) !== undefined;
+              
               log('proxy.bypass', {byPass, asset});
+
+              // if it's a webpack asset then we bypass the proxy, otherwise continue on to
+              // devServer.proxy.router
               return byPass && request.path;
           }
         },
